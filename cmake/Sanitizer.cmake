89ffc3f00f031389d4cffd5eeb79fa51b345605f
Adam Stepan <adam.stepan@firma.seznam.cz>
2019-09-21 17:55:19 +0200

    feat: allow usage of sanitizer

---
+++ cmake/Sanitizer.cmake
@@ -0,0 +1,6 @@
+if(NOT "${CMAKE_C_COMPILER_ID}" MATCHES "[Cc]lang")
+    message(FATAL_ERROR "Sanitizer is currently supported only by clang")
+endif()
+
+add_compile_options(-fsanitize=address -fno-omit-frame-pointer)
+add_link_options(-fsanitize=address)
